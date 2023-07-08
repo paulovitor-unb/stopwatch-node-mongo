a5a7a3832ad64145585e003df190b36eed623bf0
Paulo Vitor dos Santos <paulo.santos@run2biz.com>
2023-07-08 20:33:10 -0300

    Create connection with MongoDB

---
+++ src/database/dbConnection.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+
+const dbURL = process.env.DB_URL;
+
+try {
+    mongoose.connect(dbURL);
+
+    const { connection } = mongoose;
+    connection.once("connected", () => {
+        console.log("Conexão com banco de dados feita com sucesso!");
+    });
+    connection.on("disconnected ", () => {
+        console.log("Conexão com banco de dados perdida!");
+    });
+    connection.on("reconnected ", () => {
+        console.log("Conexão com banco de dados reestabelecida!");
+    });
+    connection.on("error", () => {
+        console.log("Erro de conexão com o banco de dados!");
+    });
+} catch (error) {
+    console.log(error);
+}
+
+export default mongoose;
---
+++ src/database/models.js
@@ -0,0 +1,41 @@
+import mongoose from "./dbConnection.js";
+
+const schemas = {
+    userSchema: new mongoose.Schema(
+        { name: { type: String, required: true } },
+        { timestamps: true }
+    ),
+    projectSchema: new mongoose.Schema(
+        { name: { type: String, required: true } },
+        { timestamps: true }
+    ),
+    timeSchema: new mongoose.Schema(
+        {
+            User: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+                required: true
+            },
+            Project: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Project",
+                required: true
+            },
+            time: { type: Number, min: 1 },
+            formattedTime: {
+                type: String,
+                required: true,
+                match: /^\d{2}:\d{2}:\d{2}$/
+            }
+        },
+        { createdAt: true }
+    )
+};
+
+const models = {
+    User: mongoose.model("User", schemas.userSchema),
+    Project: mongoose.model("Project", schemas.projectSchema),
+    Time: mongoose.model("Time", schemas.timeSchema)
+};
+
+export default models;
