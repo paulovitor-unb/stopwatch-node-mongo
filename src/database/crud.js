47725af6cd3f2e4c83aafacb31bd54dce90a2d6c
Paulo Vitor dos Santos <paulo.santos@run2biz.com>
2023-07-09 00:38:21 -0300

    Implement function to create new document in the database + Implement function to populate documents

--- src/database/crud.js
+++ src/database/crud.js
@@ -3,12 +3,31 @@ import models from "../database/models.js";
 const crud = {
     create: async (data, modelName) => {
         try {
-            const newDocument = await models[modelName].save(data);
+            const document = new models[modelName](data);
+            const newDocument = await document.save();
+
+            if (populateData[modelName]?.length > 0) {
+                await populateDocument(newDocument, modelName);
+            }
+
             return newDocument;
         } catch (error) {
             throw error;
         }
     }
+};
+
+const populateDocument = async (document, modelName) => {
+    for (const data of populateData[modelName]) {
+        await document.populate(data);
+    }
+};
+
+const populateData = {
+    Time: [
+        { path: "User", select: "-_id name" },
+        { path: "Project", select: "-_id name" }
+    ]
 };
 
 export default crud;
