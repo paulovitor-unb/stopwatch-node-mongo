972bdc5c65216588b70bf63296f67cacc72601b8
Paulo Vitor dos Santos <paulo.santos@run2biz.com>
2023-07-12 22:40:50 -0300

    Remove try catch

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -5,24 +5,20 @@ import mongoose from "mongoose";
 
 const dbURL = process.env.DB_URL;
 
-try {
-    mongoose.connect(dbURL);
+mongoose.connect(dbURL);
 
-    const { connection } = mongoose;
-    connection.once("connected", () => {
-        console.log("Conexão com banco de dados feita com sucesso!");
-    });
-    connection.on("disconnected ", () => {
-        console.log("Conexão com banco de dados perdida!");
-    });
-    connection.on("reconnected ", () => {
-        console.log("Conexão com banco de dados reestabelecida!");
-    });
-    connection.on("error", () => {
-        console.log("Erro de conexão com o banco de dados!");
-    });
-} catch (error) {
-    console.log(error);
-}
+const { connection } = mongoose;
+connection.once("connected", () => {
+    console.log("Conexão com banco de dados feita com sucesso!");
+});
+connection.on("disconnected ", () => {
+    console.log("Conexão com banco de dados perdida!");
+});
+connection.on("reconnected ", () => {
+    console.log("Conexão com banco de dados reestabelecida!");
+});
+connection.on("error", () => {
+    console.log("Erro de conexão com o banco de dados!");
+});
 
 export default mongoose;
